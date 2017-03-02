f3497294c171902587d7d6f05b17a5ad412dc1a5
Nam Nguyen <hoainamvn2014@gmail.com>
2017-03-03 06:59:14 +0700

    Update Testcase Skype
    Add Current Screen

--- TC_SkypeSignIn.java
+++ TC_SkypeSignIn.java
@@ -37,18 +37,42 @@ public class TC_SkypeSignIn {
     }
 
     @Test(priority = 1)
+    /* Testcase flow:
+    1. Input username
+    2. Press Continue
+    3. Input Password
+    4. Press Continue
+    Wait 5s
+    */
+
     public void SignInFailed() {
         WebElement username;
         WebElement btnContinue;
+        WebElement password;
+        WebElement btnContinue_AfterPassword;
 
-        // Input Wrong Username
+        String Screen1 = _driver.currentActivity();
+        System.out.print(Screen1);
+        // 1. Input Username
+        //_driver.findElementById("com.skype.raider:id/sign_in_userid").sendKeys("nam2003");
         username = (WebElement) _driver.findElementsByClassName("android.widget.EditText").get(0);
         username.sendKeys("nam.nguyen2003");
-        // Press Continue Arrow
-        btnContinue = (WebElement) _driver.findElementsByClassName("android.widget.TextView").get(1);
-        btnContinue.click();
-        // Wait until Password field appears
-        WebDriverWait waitingTime = new WebDriverWait(_driver,5);
+        // 2. Press Continue
+        _driver.findElementById("com.skype.raider:id/sign_in_next_btn").click();
+        //btnContinue = (WebElement) _driver.findElementsByClassName("android.widget.TextView").get(2);
+        //btnContinue.click();
+        // Take element from current page
+        String Screen2 = _driver.currentActivity();
+        // 3. Input Password
+        _driver.findElementById("com.skype.raider:id/signin_password").sendKeys("123qwe789");
+        //password = (WebElement)_driver.findElementsByClassName("android.widget.EditText").get(1);
+        //password.sendKeys("123qwe789!");
+        // 4. Press Continue
+        _driver.findElementById("com.skype.raider:id/sign_in_btn").click();
+        //btnContinue_AfterPassword = (WebElement)_driver.findElementsByClassName("android.widget.TextView").get(2);
+        //btnContinue_AfterPassword.click();
+        // 5. Wait 5s
+        WebDriverWait wait5s = new WebDriverWait(_driver,5);
     }
 
     @Test(priority = 2)
@@ -58,18 +82,18 @@ public class TC_SkypeSignIn {
         WebElement btnContinue2nd;
 
         // Find and Clear Username Field
-        username_secondTime = (WebElement) _driver.findElementsByClassName("android.widget.EditText").get(2);
+        username_secondTime = (WebElement) _driver.findElementsByClassName("android.widget.EditText").get(1);
         username_secondTime.clear();
         username_secondTime.sendKeys("namnguyen2003");
 
         // Fine ARROW to Continue
-        btnContinue2nd = (WebElement) _driver.findElementsByClassName("android.widget.TextView").get(3);
+        btnContinue2nd = (WebElement) _driver.findElementsByClassName("android.widget.TextView").get(2);
         btnContinue2nd.click();
         // Wait until Password filed appears
         WebDriverWait watingTime = new WebDriverWait(_driver, 5);
         // Input Password
-        password = (WebElement)_driver.findElementsByClassName("android.widget.EditText").get(4);
-        password.sendKeys("Abc123@@");
+        password = (WebElement)_driver.findElementsByClassName("android.widget.EditText").get(3);
+        password.sendKeys("123qwe789!");
         // Find ARROW to Continue
         btnContinue2nd.click();
     }
