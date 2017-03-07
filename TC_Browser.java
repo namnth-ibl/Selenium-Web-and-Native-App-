5f7cf8b48048aa365e3966b82a326df8a53f1afc
Nam Nguyen <hoainamvn2014@gmail.com>
2017-03-07 23:47:55 +0700

    TC_Skype Web
    Update 
    Case 1: Login Failed with Skype Web

--- TC_Browser.java
+++ TC_Browser.java
@@ -7,15 +7,22 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+import static  com.thoughtworks.selenium.SeleneseTestCase.assertNotEquals;
 import java.net.URL;
 
 public class TC_Browser {
     AndroidDriver driver;
+    WebElement btnSignIn;
+    WebElement iconSkype;
+    WebElement userName;
+    WebElement password;
 
     @BeforeTest
     public  void  BeforeTest() throws MalformedURLException
@@ -23,7 +30,6 @@ public class TC_Browser {
         DesiredCapabilities cap = new DesiredCapabilities();
 
         cap.setCapability("platformName","ANDROID");
-        // Bluestacks's name on CMD
         cap.setCapability("deviceName","emulator-5554");
         cap.setCapability(MobileCapabilityType.APP,"Browser");
 
@@ -33,9 +39,27 @@ public class TC_Browser {
     @Test
     public void LauchBrowser() throws MalformedURLException
     {
-        driver.navigate().to("https://www.google.com.vn");
-        // Inspect Element to find element 
-        driver.findElementById("lst-ib").sendKeys("Appium");
-       driver.findElementById("lst-ib").sendKeys(Keys.ENTER);
+        String screenHome = driver.currentActivity();
+        driver.navigate().to("https://www.skype.com");
+        btnSignIn = (WebElement) driver.findElementByClassName("title");
+        btnSignIn.click();
+        iconSkype = (WebElement) driver.findElementByClassName("icon icon-skype");
+        iconSkype.click();
+        WebDriverWait wait_01 = new WebDriverWait(driver,10);
+
+        String screenUsername = driver.currentActivity();
+        assertNotEquals(screenHome,screenUsername);
+        userName = (WebElement) driver.findElementByClassName("placeholder");
+        userName.sendKeys("nam.nguyen2003");
+        driver.findElementById("idSIButton9").click();
+        WebDriverWait wait_02 = new WebDriverWait(driver,5);
+
+        String screenPassword = driver.currentActivity();
+        assertNotEquals(screenUsername,screenPassword);
+        password = (WebElement) driver.findElementByClassName("placeholder");
+        password.sendKeys("123qwe789");
+        driver.findElementById("idSIButton9").click();
+        WebDriverWait wait_03 = new WebDriverWait(driver,5);
+
     }
 }
