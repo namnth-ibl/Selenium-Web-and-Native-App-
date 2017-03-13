a310465b2e706e1bf9344a973f8fe587dd2120ab
Nam Nguyen <hoainamvn2014@gmail.com>
2017-03-13 23:50:38 +0700

    Update: Testcase - Login Failed
    Today 13/3/2017
    - Testcase Login Failed - OK
    - Testcase Login Success - Failed
    Issues: 
    - Cannot locate back button
    Solution/Plan
    - Back() and wait until Back button appears

--- TC_Browser.java
+++ TC_Browser.java
@@ -4,9 +4,12 @@
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -14,6 +17,8 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import static  com.thoughtworks.selenium.SeleneseTestCase.assertNotEquals;
+import static junit.framework.Assert.assertEquals;
+
 import java.net.URL;
 
 public class TC_Browser {
@@ -36,21 +41,17 @@ public class TC_Browser {
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
     }
 
-    @Test
+    @Test (priority = 1)
     public void LoginFailed() throws MalformedURLException
     {
-        //String screenHome = driver.currentActivity();
         //Run Browser and go to URL
         driver.navigate().to("https://www.skype.com");
-        btnSignIn = (WebElement) driver.findElementByClassName("title");
+        btnSignIn = driver.findElementByClassName("title");
         btnSignIn.click();
-        iconSkype = (WebElement) driver.findElementByLinkText("Use Skype online");
+        iconSkype = driver.findElementByLinkText("Use Skype online");
         iconSkype.click();
         WebDriverWait wait_01 = new WebDriverWait(driver,10);
 
-        //String screenUsername;
-        //screenUsername = driver.currentActivity();
-        //assertNotEquals(screenHome,screenUsername);
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -61,14 +62,25 @@ public class TC_Browser {
         driver.findElementById("signIn").click();
         WebDriverWait wait_02 = new WebDriverWait(driver,5);
 
-        //String screenPassword;
-        //screenPassword = driver.currentActivity();
-        //assertNotEquals(screenUsername,screenPassword);
-        password = (WebElement) driver.findElementByClassName("placeholder");
+        password = driver.findElementById("i0118");
         password.sendKeys("123qwe789");
         driver.findElementById("idSIButton9").click();
-        WebDriverWait wait_03 = new WebDriverWait(driver,5);
+
     }
+
+//    @Test (priority = 2)
+//    public  void LoginSuccess()
+//    {
+//        WebDriverWait wait_03 = new WebDriverWait(driver,5);
+//        wait_03.until(ExpectedConditions.elementToBeClickable(By.id("idBtn_Back")));
+//        driver.findElementById("idBtn_Back").click();
+//        driver.findElementById("i0116").clear();
+//        driver.findElementById("i0116").sendKeys("namnguyen2003");
+//        driver.findElementById("idSIButton9").click();
+//        driver.findElementById("i0118").sendKeys("123qwe789!");
+//        driver.findElementById("idSIButton9").click();
+//        WebDriverWait wait_04 = new WebDriverWait(driver,30);
+//    }
 
     @AfterTest
     public void AfterTest()
