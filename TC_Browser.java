2df00ac753e6b08465a1ba2fa56f5db1a86582cd
Nam Nguyen <hoainamvn2014@gmail.com>
2017-03-15 13:59:27 +0700

    Update: Testcase - Login Success
    Today: 15/3/2017
    - Develope: Testcase - Login Success
    Plan:
    - Chat

--- TC_Browser.java
+++ TC_Browser.java
@@ -46,41 +46,51 @@ public class TC_Browser {
     {
         //Run Browser and go to URL
         driver.navigate().to("https://www.skype.com");
+        // Click to Select Sign Type
         btnSignIn = driver.findElementByClassName("title");
         btnSignIn.click();
+        // Click to "Use Skype Online"
         iconSkype = driver.findElementByLinkText("Use Skype online");
         iconSkype.click();
+        // Waiting for page redirect
         WebDriverWait wait_01 = new WebDriverWait(driver,10);
-
+        // Waiting for the page finish loading
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        // Input username
         userName = driver.findElementById("username");
         userName.sendKeys("nam.nguyen2003");
+        // Click Signin next step
         driver.findElementById("signIn").click();
         WebDriverWait wait_02 = new WebDriverWait(driver,5);
-
+        // Input password
         password = driver.findElementById("i0118");
         password.sendKeys("123qwe789");
+        // Click Signin
         driver.findElementById("idSIButton9").click();
-
+        WebDriverWait wait_03 = new WebDriverWait(driver,5);
     }
 
-//    @Test (priority = 2)
-//    public  void LoginSuccess()
-//    {
-//        WebDriverWait wait_03 = new WebDriverWait(driver,5);
-//        wait_03.until(ExpectedConditions.elementToBeClickable(By.id("idBtn_Back")));
-//        driver.findElementById("idBtn_Back").click();
-//        driver.findElementById("i0116").clear();
-//        driver.findElementById("i0116").sendKeys("namnguyen2003");
-//        driver.findElementById("idSIButton9").click();
-//        driver.findElementById("i0118").sendKeys("123qwe789!");
-//        driver.findElementById("idSIButton9").click();
-//        WebDriverWait wait_04 = new WebDriverWait(driver,30);
-//    }
+    @Test (priority = 2)
+    public  void LoginSuccess()
+    {
+        // Back to Input Username Page
+        driver.navigate().back();
+        driver.navigate().back();
+        // Input username
+        driver.findElementById("username").sendKeys("namnguyen2003");
+        // Click Signin
+        driver.findElementById("signIn").click();
+        // Input password
+        driver.findElementById("i0118").sendKeys("123qwe789!");
+        // Click Signin
+        driver.findElementById("idSIButton9").click();
+        // Wait for 30 seconds
+        WebDriverWait wait_04 = new WebDriverWait(driver,30);
+    }
 
     @AfterTest
     public void AfterTest()
