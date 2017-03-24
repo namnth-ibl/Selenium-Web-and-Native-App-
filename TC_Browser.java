826fec8b804f82a8b7229a27faef02a80457f586
Nam Nguyen <hoainamvn2014@gmail.com>
2017-03-24 10:37:28 +0700

    Fixed Testcase 2 - Login Success
    Using
    + findElementByName with elements which is the same to previous Pages.

--- TC_Browser.java
+++ TC_Browser.java
@@ -16,8 +16,8 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
-import static  com.thoughtworks.selenium.SeleneseTestCase.assertNotEquals;
-import static junit.framework.Assert.assertEquals;
+//import static  com.thoughtworks.selenium.SeleneseTestCase.assertNotEquals;
+//import static junit.framework.Assert.assertEquals;
 
 import java.net.URL;
 
@@ -46,32 +46,25 @@ public class TC_Browser {
     {
         //Run Browser and go to URL
         driver.navigate().to("https://www.skype.com");
-        // Click to Select Sign Type
         btnSignIn = driver.findElementByClassName("title");
         btnSignIn.click();
-        // Click to "Use Skype Online"
         iconSkype = driver.findElementByLinkText("Use Skype online");
         iconSkype.click();
-        // Waiting for page redirect
         WebDriverWait wait_01 = new WebDriverWait(driver,10);
-        // Waiting for the page finish loading
+
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        // Input username
         userName = driver.findElementById("username");
-        userName.sendKeys("nam.nguyen2003");
-        // Click Signin next step
+        userName.sendKeys("nam.nguyen2005");
         driver.findElementById("signIn").click();
         WebDriverWait wait_02 = new WebDriverWait(driver,5);
-        // Input password
+
         password = driver.findElementById("i0118");
         password.sendKeys("123qwe789");
-        // Click Signin
         driver.findElementById("idSIButton9").click();
-        WebDriverWait wait_03 = new WebDriverWait(driver,5);
     }
 
     @Test (priority = 2)
@@ -81,11 +74,13 @@ public class TC_Browser {
         driver.navigate().back();
         driver.navigate().back();
         // Input username
-        driver.findElementById("username").sendKeys("namnguyen2003");
+        //driver.findElementById("i0116").sendKeys("namnguyen2003");
+        driver.findElementByName("loginfmt").sendKeys("nam.nth1827");
         // Click Signin
-        driver.findElementById("signIn").click();
+        driver.findElementById("idSIButton9").click();
+        WebDriverWait wait_05 = new WebDriverWait(driver,5);
         // Input password
-        driver.findElementById("i0118").sendKeys("123qwe789!");
+        driver.findElementByName("passwd").sendKeys("Abc123!");
         // Click Signin
         driver.findElementById("idSIButton9").click();
         // Wait for 30 seconds
