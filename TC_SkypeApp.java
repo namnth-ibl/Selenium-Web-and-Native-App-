889d0435a0d5465bf594f8fe2c1145a519f799e5
Nam Nguyen <hoainamvn2014@gmail.com>
2017-04-10 23:16:09 +0700

    Add testcase, Switch Context
    - Add testcase Register
    - Switch Context to WEBVIEW (in progress)

--- TC_SkypeApp.java
+++ TC_SkypeApp.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import static com.thoughtworks.selenium.SeleneseTestCase.assertNotEquals;
 import static junit.framework.Assert.assertEquals;
 
@@ -20,8 +21,9 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import  java.util.Collection;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 
 public class TC_SkypeApp {
@@ -41,14 +43,13 @@ public class TC_SkypeApp {
         // Device name
         _cap.setCapability("deviceName", "emulator-5554");
         // Package Name
-        _cap.setCapability("packageName","com.skype.raider");
+        _cap.setCapability("packageName", "com.skype.raider");
         // App Activity
-        _cap.setCapability("appActivity","com.skype.android.app.main.SplashActivity");
+        _cap.setCapability("appActivity", "com.skype.android.app.main.SplashActivity");
 
         _driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), _cap);
     }
 
-    @Test(priority = 1)
     /* Testcase flow:
     1. Input username
     2. Press Continue
@@ -56,11 +57,11 @@ public class TC_SkypeApp {
     4. Press Continue
     Wait 5s
     */
-
+    @Test(priority = 1)
     public void SignInFailed() {
         String screenUserName = _driver.currentActivity();
-        WebDriverWait waitToPresent = new WebDriverWait(_driver,10);
-
+        WebDriverWait waitToPresent = new WebDriverWait(_driver, 10);
+        // Wait until element is presented
         waitToPresent.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.skype.raider:id/sign_in_userid")));
 
         // 1. Input Username
@@ -69,11 +70,11 @@ public class TC_SkypeApp {
         // 2. Press Continue
         _driver.findElementById("com.skype.raider:id/sign_in_next_btn").click();
 
-        WebDriverWait wait_1st = new WebDriverWait(_driver,10);
+        WebDriverWait wait_1st = new WebDriverWait(_driver, 10);
         wait_1st.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.skype.raider:id/signin_password")));
 
         String screenPassword = _driver.currentActivity();
-        assertNotEquals(screenUserName,screenPassword);
+        assertNotEquals(screenUserName, screenPassword);
 
         // 3. Input Password
         _driver.findElementById("com.skype.raider:id/signin_password").sendKeys("******");
@@ -81,41 +82,65 @@ public class TC_SkypeApp {
         _driver.findElementById("com.skype.raider:id/sign_in_btn").click();
 
         // 5. Wait 5s
-        WebDriverWait wait_2nd = new WebDriverWait(_driver,10);
+        WebDriverWait wait_2nd = new WebDriverWait(_driver, 10);
         wait_2nd.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.skype.raider:id/error_message")));
         // Get Error Message
         String errorMess = _driver.findElementById("com.skype.raider:id/error_message").getText();
         // Check error message is the same
-        assertEquals(errorMess,"Oops, please check your details.");
+        assertEquals(errorMess, "Oops, please check your details.");
 
     }
 
-//    @Test(priority = 2)
+    @Test(priority = 2)
+    public void Register() {
+        _driver.navigate().back();
+        // Switch context to WEBVIEW
+        String currContext = _driver.getContext(); // will this return NATIVE_APP ?
+        _driver.context("WEBVIEW");
+        _driver.context(currContext);
+        // Second Way
+//        Set<String> contextNames = _driver.getContextHandles();
+//        for (String contextName : contextNames) {
+//            System.out.println(contextNames); //prints out something like NATIVE_APP \n WEBVIEW_1
+//        }
+//        _driver.context(contextNames.toArray()[1]); // set context to WEBVIEW_1
+
+        // Part 1 of Register
+        _driver.findElementById("com.skype.raider:id/create_account_label").click();
+        WebDriverWait wait_for_register = new WebDriverWait(_driver,10);
+        wait_for_register.until(ExpectedConditions.visibilityOfElementLocated(By.id("MemberName")));
+        _driver.findElementById("MemberName").sendKeys("1668333911");
+        _driver.findElementById("Password").sendKeys("123qwe789");
+        _driver.findElementById("iSignupAction").click();
+        WebDriverWait wait_for_next_regiter = new WebDriverWait(_driver,5);
+        // Part 2 of Register
+        _driver.findElementById("FirstName").sendKeys("Nam");
+        _driver.findElementById("LastName").sendKeys("Nguyen");
+        _driver.findElementById("iSignupAction").click();
+        WebDriverWait wait_for_verifyCode = new WebDriverWait(_driver, 5);
+        // Part 3 of Register
+        _driver.findElementById("VerificationCode").sendKeys("2985");
+        _driver.findElementById("iSignupAction").click();
+        WebDriverWait wait_for_process = new WebDriverWait(_driver,10);
+    }
+
+//    @Test(priority = 3)
 //    public void SignInSuccess() {
-//        WebElement username_secondTime;
-//        WebElement password;
-//        WebElement btnContinue2nd;
-//
-//        // Find and Clear Username Field
-//        username_secondTime = (WebElement) _driver.findElementsByClassName("android.widget.EditText").get(1);
-//        username_secondTime.clear();
-//        username_secondTime.sendKeys("namnguyen2");
-//
-//        // Fine ARROW to Continue
-//        btnContinue2nd = (WebElement) _driver.findElementsByClassName("android.widget.TextView").get(2);
-//        btnContinue2nd.click();
-//        // Wait until Password filed appears
-//        WebDriverWait watingTime = new WebDriverWait(_driver, 5);
+//        // Clear content of Username field
+//        _driver.findElementById("com.skype.raider:id/signin_skypename").clear();
+//        // Input Username
+//        _driver.findElementById("com.skype.raider:id/signin_skypename").sendKeys("namnguyen2003");
+//        // Clear content of Password field
+//        _driver.findElementById("com.skype.raider:id/signin_password").clear();
 //        // Input Password
-//        password = (WebElement)_driver.findElementsByClassName("android.widget.EditText").get(3);
-//        password.sendKeys("******");
-//        // Find ARROW to Continue
-//        btnContinue2nd.click();
+//        _driver.findElementById("com.skype.raider:id/signin_password").sendKeys("123qwe789!");
+//        // Click button Continue
+//        _driver.findElementById("com.skype.raider:id/sign_in_btn").click();
+//        WebDriverWait wait_3rd = new WebDriverWait(_driver, 10);
 //    }
 
     @AfterTest
-    public  void QuitDriver()
-    {
+    public void QuitDriver() {
         _driver.quit();
     }
 }
