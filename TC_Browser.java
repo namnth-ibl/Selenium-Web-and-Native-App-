9fbbf1479b0ce97ff35dad18732062e0927e41cc
Nam Nguyen <hoainamvn2014@gmail.com>
2017-05-29 21:46:28 +0700

    Execute test on Firefox browser

--- TC_Browser.java
+++ TC_Browser.java
@@ -1,95 +1,56 @@
-/**
- * Created by Nam on 3/5/2017.
- */
+package TC_WEB;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.net.MalformedURLException;
-//import static  com.thoughtworks.selenium.SeleneseTestCase.assertNotEquals;
-//import static junit.framework.Assert.assertEquals;
-
-import java.net.URL;
-
+/**
+ * Created by Nam on 5/25/2017.
+ */
 public class TC_Browser {
-    AndroidDriver driver;
-    WebElement btnSignIn;
-    WebElement iconSkype;
-    WebElement userName;
-    WebElement password;
 
-    @BeforeTest
-    public  void  BeforeTest() throws MalformedURLException
+    public  static  void main (String[] args )
     {
-        DesiredCapabilities cap = new DesiredCapabilities();
+        String geckoPath = "C://Users//Nam//FireFoxDriver//geckodriver.exe";
+        System.setProperty("webdriver.gecko.driver",geckoPath);
 
-        cap.setCapability("platformName","ANDROID");
-        cap.setCapability("deviceName","emulator-5554");
-        // Using Default Browser of device
-        cap.setCapability(MobileCapabilityType.APP,"Browser");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
-    }
-
-    @Test (priority = 1)
-    public void LoginFailed() throws MalformedURLException
-    {
-        //Run Browser and go to URL
-        driver.navigate().to("https://www.skype.com");
-        btnSignIn = driver.findElementByClassName("title");
-        btnSignIn.click();
-        iconSkype = driver.findElementByLinkText("Use Skype online");
-        iconSkype.click();
-        WebDriverWait wait_01 = new WebDriverWait(driver,10);
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        userName = driver.findElementById("username");
-        userName.sendKeys("Abc123");
-        driver.findElementById("signIn").click();
-        WebDriverWait wait_02 = new WebDriverWait(driver,5);
-
-        password = driver.findElementById("i0118");
-        password.sendKeys("******");
-        driver.findElementById("idSIButton9").click();
-    }
-
-    @Test (priority = 2)
-    public  void LoginSuccess()
-    {
-        // Back to Input Username Page
-        driver.navigate().back();
-        driver.navigate().back();
-        // Input username
-        //driver.findElementById("i0116").sendKeys("namnguyen2003");
-        driver.findElementByName("loginfmt").sendKeys("AbcCab");
-        // Click Signin
-        driver.findElementById("idSIButton9").click();
-        WebDriverWait wait_05 = new WebDriverWait(driver,5);
-        // Input password
-        driver.findElementByName("passwd").sendKeys("******");
-        // Click Signin
-        driver.findElementById("idSIButton9").click();
-        // Wait for 30 seconds
-        WebDriverWait wait_04 = new WebDriverWait(driver,30);
-    }
-
-    @AfterTest
-    public void AfterTest()
-    {
-        driver.quit();
+        WebDriver driver = new FirefoxDriver();
+        // Enter link
+        driver.get("https://www.foody.vn");
+        driver.getTitle();
+        // Click "Đăng Nhập"
+        driver.findElement(By.xpath("//*[@id=\"login_form\"]/div[1]/div/a/span")).click();
+        // Enter "Email"
+        driver.findElement(By.id("Email")).sendKeys("nam@gmail.com");
+        // Enter "Password"
+        driver.findElement(By.id("Password")).sendKeys("123qw234");
+        // Click "Đăng Nhập"
+        driver.findElement(By.id("signin_submit")).click();
+        // Close popup of Firefox
+        Alert alertPopup = driver.switchTo().alert();
+        alertPopup.dismiss();
+        // Click "Đăng Ký" link
+        driver.findElement(By.className("register")).click();
+        // Click "Dăng Ký" button without information
+        driver.findElement(By.xpath("//*[@id=\"registerForm\"]/table/tbody/tr[10]/td[2]/button")).click();
+        // Enter "Tên"
+        driver.findElement(By.id("FirstName")).sendKeys("Nam");
+        // Enter "Họ"
+        driver.findElement(By.id("LastName")).sendKeys("Nguyễn");
+        // Enter "Email"
+        driver.findElement(By.id("register_email")).sendKeys("hoainamvn2014@gmail.com");
+        // Click "Chọn tỉnh/thành"
+        driver.findElement(By.id("StateProvinceId")).click();
+        // Select "TP. HCM"
+        driver.findElement(By.xpath("//*[@id=\"StateProvinceId\"]/option[2]")).click();
+        // Enter "Mật Khẩu"
+        driver.findElement(By.id("register_pass")).sendKeys("lamjcopassAnh2");
+        // Enter "Nhập Lại Mật Khẩu"
+        driver.findElement(By.id("ConfrimPassword")).sendKeys("lamjcopassAnh2");
+        // Check "I'm not a robot"
+        driver.findElement(By.xpath("//*[@id=\"recaptcha-anchor\"]/div[5]")).click();
+        // Click "Đăng Ký"
+        driver.findElement(By.xpath("//*[@id=\"registerForm\"]/table/tbody/tr[10]/td[2]/button")).click();
     }
 }
