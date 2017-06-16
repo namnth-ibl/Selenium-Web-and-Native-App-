fd132d4631eb75795cc263819fca3b7a515b01ae
Nam Nguyen <hoainamvn2014@gmail.com>
2017-06-16 08:11:23 +0700

    Fix: PackageName is wrong

--- TC_StixChat.java
+++ TC_StixChat.java
@@ -1,7 +1,7 @@
-package TC_STIXCHAT_APP;
+package StixChat;
 
 /**
- * Created by Nam on 6/7/2017.
+ * Created by Administrator on 6/12/2017.
  */
 
 import io.appium.java_client.android.AndroidDriver;
@@ -11,9 +11,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import static junit.framework.Assert.assertEquals;
-import static org.testng.Assert.assertNotEquals;
 
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
@@ -26,46 +23,67 @@ import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
+
 
 public class TC_StixChat {
 
     AndroidDriver driver;
-//    String appName = "StixChat.apk";
-//    File appPath = new File("src/main/resources/" + appName);
+
     @BeforeTest
-    public  void  BeforeTest() throws MalformedURLException
+    public void BeforeTest() throws MalformedURLException
     {
         DesiredCapabilities cap = new DesiredCapabilities();
-
-//        cap.setCapability("App",appPath);
-        cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"ANDROID");
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"4d00922fb8f060fb");
-        cap.setCapability(MobileCapabilityType.APP_PACKAGE,"com.stixchat.app");
-        cap.setCapability(MobileCapabilityType.APP_ACTIVITY,"com.gnt.stixchat.LandingActivity");
-
+        // Information of App and Device
+        String platformName = "ANDROID";
+        String deviceName   = "d15c51e4";
+        String packageName  = "com.stixchat.app";
+        String appActivity  = "com.gnt.stixchat.LandingActivity";
+        // File .APK
+        String appName = "Stixchat.apk";
+        File appPath = new File("src/main/resources/" + appName);
+        // Setting Capabilities
+        cap.setCapability(MobileCapabilityType.APP,appPath);
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME,platformName);
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME,deviceName);
+        cap.setCapability(MobileCapabilityType.APP_PACKAGE,packageName);
+        cap.setCapability(MobileCapabilityType.APP_ACTIVITY,appActivity);
+        // Connect to Appium Server
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
     }
 
     @Test
-    public  void LaunchApp()
+    public  void  LaunchApp()
     {
+        // Get current Activity
+        String currScreen = driver.currentActivity();
+        System.out.println("Current Screen: " + currScreen);
+        // Click GET STARTED
         driver.findElementById("com.stixchat.app:id/signUp").click();
-        System.out.println("Đã click Get Started.");
+        System.out.println("Clicked GET STARTED.");
+        // Click COUNTRY DropDownList
         driver.findElementById("com.stixchat.app:id/txtCountry").click();
-        System.out.println("Đã click Country");
+        System.out.println("Clicked Drop Down List.");
+        // Enter COUNTRY "vi"
         driver.findElementById("com.stixchat.app:id/search_src_text").sendKeys("vi");
-        System.out.println("Đã enter vi");
+        System.out.println("Entered vi.");
+        // Select "Vietnam"
         driver.findElementByName("Vietnam").click();
-        System.out.println("Đã chọn Vietnam");
+        System.out.println("Clicked Vietnam.");
+        // Enter Phone Number
         driver.findElementById("com.stixchat.app:id/editTextPhoneNumber").sendKeys("1999888777");
-        System.out.println("Đã enter sdt");
+        System.out.println("Entered 1999888777.");
+        // Click NEXT
         driver.findElementById("com.stixchat.app:id/btnNext").click();
-        System.out.println("Đã click Next");
+        System.out.println("Clicked NEXT.");
+        WebDriverWait wait_1st = new WebDriverWait(driver,5);
     }
 
     @AfterTest
-    public  void Aftertest(){
+    public void AfterTest()
+    {
+        // Quit driver for closing app
         driver.quit();
+        System.out.println("Driver is quit.");
     }
+
 }
